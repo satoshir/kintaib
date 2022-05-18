933b4842aa653178baf455ee2dd8dd69712dfe51
satoshir <nikken.1215@gmail.com>
2022-05-18 11:11:36 +0000

    Initialize repository

---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  root 'static_pages#top'
+  get '/signup', to: 'users#new'
+
+  # ログイン機能
+  get    '/login', to: 'sessions#new'
+  post   '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+  resources :users do
+    member do
+      get 'edit_basic_info'
+      patch 'update_basic_info'
+      get 'attendances/edit_one_month'
+      patch 'attendances/update_one_month'
+    end
+    resources :attendances, only: :update
+  end
+end
