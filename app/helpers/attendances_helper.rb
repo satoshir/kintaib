6bc0e2d64216964a07c6232823629ca5e9c60400
satoshir <nikken.1215@gmail.com>
2022-06-02 14:28:11 +0000

    kintaiB F

--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -14,4 +14,8 @@ module AttendancesHelper
   def working_times(start, finish)
     format("%.2f", (((finish - start) / 60) / 60.0))
   end
+  
+  def format_min(time)
+    format("%.2d",((time.strftime('%M').to_i / 15).round) * 15)
+  end
 end
